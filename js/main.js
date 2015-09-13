28099f69e7ef32d5f1d50ec35bbf4f89cd2c6f37
yinchuandong <yincd520@sina.com>
2015-09-13 20:57:45 +0800

    完成目前浏览页面id的保存

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 var PageMain = {
     imgUrl: './images/',
-    imgArr: ["3g.png", "4k.png", "android.png", "arrow.png", "back.png", "bg1.png", "bg2.png", "btn-back.png", "btn-buy.png", "cf.png", "circle-android.png", "circle-microsoft.png", "cmp.png", "detail_bg.png", "ie.png", "index-product1.png", "index-product2.png", "index-product3.png", "index-product4.png", "list.png", "locate.png", "logo.png", "lol.png", "modernwar.png", "nfs.png", "office.png", "pdf.png", "picture1.png", "picture2-1.png", "picture2.png", "picture3-1.png", "picture3-2.png", "picture3-3.png", "picture3.png", "picture4-1.png", "picture4-2.png", "picture4.png", "picture5-1.png", "picture5-2.png", "picture5.png", "picture6-1.png", "picture6-2.png", "picture6.png", "picture7.png", "picture8-1.png", "picture8-2.png", "picture8.png", "product1.png", "product2.png", "product3.png", "product4.png", "ps.png", "qq.png", "qqcar.png", "sky.png", "sxd.png", "text.png", "tick.png", "title.png", "wechat.png", "weibo.png", "windows.png", "youku.png"],
+    imgArr: ["android.png", "arrow.png", "bg1.png", "bg2.png", "btn-back.png", "btn-buy.png", "circle-android.png", "circle-microsoft.png", "detail_bg.png", "index-product1.png", "index-product2.png", "index-product3.png", "index-product4.png", "list.png", "logo.png", "picture1.png", "text.png", "title.png", "windows.png"],
 
+    initIndex: 0,
 
     preloadImg: function() {
         var self = this;
-        // var images = self.imgArr;
-        var images = [];
+        var images = self.imgArr;
+        // var images = [];
         var imgLen = images.length;
         if (window.devicePixelRatio >= 1.25) {
             for (var i = 0; i < imgLen; i++) {
@@ -18,19 +19,37 @@ var PageMain = {
         // console.log(self.imgArr)
 
         seajs.use('./js/imageLoader.js', function(loader) {
-            // loader(images, function(percent) {
+            loader(images, function(percent) {
 
                 //加载完一张图片之后的回调函数
-                // $('.load-percent').text(Math.floor(percent * 100) + '%');
-                // if (percent == 1) {
+                $('.load-percent').text(Math.floor(percent * 100) + '%');
+                if (percent == 1) {
                     //加载完所有图片之后
+                    var hash = window.location.hash == '' ? '#index=0' : window.location.hash;
+                    hash = hash.substr(1); //remove '#'
+                    var params = self.parseUrl(hash);
+                    self.initIndex = parseInt(params['index']);
                     self.initPageScroll();
-                    self.initPageOne();
-                // }
-            // });
+                    self.initPageById(self.initIndex);
+                }
+            });
 
         });
 
+    },
+
+    parseUrl: function(str) {
+        var map = {};
+        var pairs = str.split('&');
+        for (var i = 0; i < pairs.length; i++) {
+            var arr = pairs[i].split('=');
+            if (arr.length == 2) {
+                map[arr[0]] = arr[1];
+            } else {
+                map[arr[0]] = '';
+            }
+        }
+        return map;
     },
 
     initPageScroll: function() {
@@ -44,35 +63,43 @@ var PageMain = {
                 var $sectionSelector = $('#j-panel-main > section');
                 // debugger
                 PageScroll.init({
+                    initIndex: self.initIndex,
                     container: '#j-panel-main',
                     sectionSelector: 'section',
                     scrollingSpeed: 800, // 滚动速度
                     scrollDelay: 0,
                     afterScroll: function(o) { //下一页加载完成之后回调函数（active类已在此之前默认添加）
+                        //change the hash of url to save the old index
+                        window.location.hash = 'index=' + o.index;
                         $sectionSelector.slice(0, o.index + 4).show();
                         // debugger
-                        switch (o.index) {
-                            case 0:
-                                self.initPageOne();
-                                break;
-                            case 1:
-                                self.initPageTwo();
-                                break;
-                            case 2:
-                                self.initPageThree();
-                                break;
-                            default:
-                                self.initPageOne();
-                                break;
-                        }
+                        self.initPageById(o.index);
                     }
                 });
-
+                // debugger
                 $(document).on('click', '.arrow', function(evt) {
                     PageScroll.scrollDown();
                 });
             });
         });
+    },
+
+    initPageById: function(index) {
+        var self = this;
+        switch (index) {
+            case 0:
+                self.initPageOne();
+                break;
+            case 1:
+                self.initPageTwo();
+                break;
+            case 2:
+                self.initPageThree();
+                break;
+            default:
+                self.initPageOne();
+                break;
+        }
     },
 
     initPageOne: function() {
@@ -99,7 +126,7 @@ var PageMain = {
         });
     },
 
-     initPageThree: function() {
+    initPageThree: function() {
         $(".android").removeClass("android-animate");
         $(".windows").removeClass("windows-animate");
         $(".text").removeClass("text-animate");
